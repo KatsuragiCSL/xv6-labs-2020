488ef9855d372f28d3c6651b749bad53502e3c01
KatsuragiCSL <shue12345678@gmail.com>
2022-08-19 01:51:31 +0800

    6.s801 lab syscall done

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,3 +80,17 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+uint64
+freemem(void)
+{
+  struct run *r;
+  r = kmem.freelist;
+
+  int ret = 0;
+  while (r) {
+    ret += PGSIZE;
+    r = r->next;
+  }
+  return ret;
+}
